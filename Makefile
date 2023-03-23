84243377dd67d04a5e0370ea073759cf7302c627
Tommaso <tommaso.giorgi@hotmail.com>
2023-03-23 17:22:06 +0000

    Added plot functionality

--- Makefile
+++ Makefile
@@ -2,7 +2,11 @@ CXX      ?= g++
 CXXFLAGS ?= -std=c++20
 CPPFLAGS ?= -O3 -Wall -I $(PWD)/include
 EXEC = main
-SRCS = $(PWD)/src/parameters.cpp $(PWD)/src/main.cpp 
+SRCS = $(PWD)/src/main.cpp 
 
 $(EXEC):
 	$(CXX) $(CXXFLAGS) $(CPPFLAGS) $(SRCS) -o $(EXEC)
+
+plot: $(EXEC)
+	./main
+	./plot.sh
---
+++ plot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# A script that launches gnuplot on the file result.dat
+#
+# you can also launch gnuplot and type the command at the gnuplot prompt
+#
+
+gnuplot<<EOF
+plot "result.dat" u 1:2 w lp lw 2 title "uh", "result.dat" u 1:3 w l lw 2title "uex"
+pause mouse keypress
+EOF
+
